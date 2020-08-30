4b01e97a62865890a5f60a0d64e944173ec02635
MichaIng <micha@dietpi.com>
2020-08-30 22:19:36 +0200

    Update URLs and use python3

--- HTPC-Manager.bat
+++ HTPC-Manager.bat
@@ -5,13 +5,13 @@ if not exist Htpc.py (
     for %%X in (git.exe) do (set FOUND=%%~$PATH:X)
     if defined FOUND (
         DEL "%~f0"
-        git clone https://github.com/Hellowlol/HTPC-Manager.git .
+        git clone https://github.com/HTPC-Manager/HTPC-Manager.git .
     ) else (
-        echo "Git not installed. Install from: http://msysgit.github.io/"
+        echo "Git not installed. Install from: https://gitforwindows.org/"
         pause
         exit
     )
 )
-start cmd /c python Htpc.py
+start cmd /c python3 Htpc.py
 exit
 )
