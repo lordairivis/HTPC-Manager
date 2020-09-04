e2edf21376d32be725a5e266e2512b939178f736
MichaIng <micha@dietpi.com>
2020-09-04 19:27:53 +0200

    Update repository for updater
    Signed-off-by: MichaIng <micha@dietpi.com>

--- htpc/updater.py
+++ htpc/updater.py
@@ -31,7 +31,7 @@ from apscheduler.triggers.interval import IntervalTrigger
 from htpc.root import do_restart
 
 # configure git repo
-gitUser = 'Hellowlol'
+gitUser = 'HTPC-Manager'
 gitRepo = 'HTPC-Manager'
 
 
@@ -229,7 +229,7 @@ class GitUpdater(object):
         #self.update_remote_origin() # Disable this since it a fork for now.
 
     def update_remote_origin(self):
-        self.git_exec(self.git, 'config remote.origin.url https://github.com/Hellowlol/HTPC-Manager.git')
+        self.git_exec(self.git, 'config remote.origin.url https://github.com/HTPC-Manager/HTPC-Manager.git')
 
     def current_branch_name(self):
         output = self.git_exec(self.git, 'rev-parse --abbrev-ref HEAD')
@@ -280,7 +280,7 @@ class GitUpdater(object):
             # If its false, default to master branch
             d["branch"] = htpc.settings.get('branch', 'master2')
 
-        branches = self.git_exec(self.git, 'ls-remote --heads https://github.com/Hellowlol/HTPC-Manager.git')
+        branches = self.git_exec(self.git, 'ls-remote --heads https://github.com/HTPC-Manager/HTPC-Manager.git')
         if branches:
             # find all branches except the current branch.
             d["branches"] = [b for b in re.findall('\S+\Wrefs/heads/(.*)', branches) if b != cbn]
